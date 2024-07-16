e5a7fa4ce6aa0a5f5a8fd2f07d1fcda6eae0e6ae
Austin Ameh <arewageek@gmail.com>
2024-07-16 22:03:38 +0100

    Telegram Miniapp autoclose resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="px-10 py-10 flex h-[80vh] w-full items-center justify-center  sticky top-10">
-      <div className="bg-white/10 px-10 pb-10 py-4 w-full lg:w-1/4 rounded-lg divide-y divide-white/30">
+    <main className="px-5 py-10 flex h-[80vh] w-full items-center justify-center sticky top-10">
+      <div className="bg-white/10 px-5 pb-10 py-4 w-full lg:w-1/4 rounded-lg divide-y divide-white/30">
         <h2 className="font-bold text-md py-2">
           Choose Your Candidates
         </h2>
